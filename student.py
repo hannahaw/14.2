5119e568f022ba1ad39868c313e2f353821f1541
hannahaw <142659698+hannahaw@users.noreply.github.com>
2026-02-27 12:56:28 +0200

    Create student.py

---
+++ student.py
@@ -0,0 +1,18 @@
+from human import Human
+
+class Student(Human):
+
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}, {self.age} years old, {self.gender}, Record book: {self.record_book}"
+
+    def __eq__(self, other):
+        if isinstance(other, Student):
+            return str(self) == str(other)
+        return False
+
+    def __hash__(self):
+        return hash(str(self))
